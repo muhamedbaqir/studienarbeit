6774c631d46613af73c3ce5c9816229deb38adb8
muhamedbaqir <muhamedbaqir32@gmail.com>
2025-02-19 21:21:40 +0100

    added test_vmalloc function

--- code/ramdisk.c
+++ code/ramdisk.c
@@ -1,28 +1,32 @@
-/* 
-
- * hello-1.c - The simplest kernel module. 
-
- */ 
-
 #include <linux/module.h> /* Needed by all modules */ 
-
 #include <linux/printk.h> /* Needed for pr_info() */ 
+#include <linux/vmalloc.h>
 
- 
+void test_vmalloc(void);
 
 int init_module(void) 
 
 { 
 
-    pr_info("Hello world 1.\n"); 
-
- 
+    pr_info("Custom ramdisk module"); 
+    test_vmalloc();
 
     /* A non 0 return means init_module failed; module can't be loaded. */ 
 
     return 0; 
 
 } 
+
+void test_vmalloc(void){
+	int size = 100;
+	char *ptr = vmalloc(size);
+	if(!ptr)pr_err("error while trying to vmalloc \n");
+	else {
+		strcpy(ptr,"hello world string");
+		pr_err("string is %s",ptr);
+		vfree(ptr);
+	}
+}
 
  
 
